54d26be804f5593888501e520955e6162d189c9e
Milad Karimi <Milad Karimi milad.karimi@eseminar.tv>
2023-07-25 01:49:23 +0330

    edit bitycle

--- src/Bitycle/Bitycle.php
+++ src/Bitycle/Bitycle.php
@@ -215,7 +215,7 @@ class Bitycle extends PortAbstract implements PortInterface
         } else {
             if ($result['code'] == 0) {
                 $this->refId = $result['data']["ref_no"];
-                $this->pamentUrl = 'https://tp.bitycle.com/assign/'.$result['data']["ref_no"];
+                $this->pamentUrl = 'https://pay.bitycle.com/assign/'.$result['data']["ref_no"];
                 $this->transactionSetRefId();
                 return true;
             }
